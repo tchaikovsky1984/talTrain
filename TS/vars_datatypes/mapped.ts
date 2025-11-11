139732bf529c20e5f11e1524f40927c931874fe8
Anshul <anshulkumarg0re@gmail.com>
2025-11-11 17:09:47 +0530

    typescript brush-up: nov 11

--- TS/vars_datatypes/mapped.ts
+++ TS/vars_datatypes/mapped.ts
@@ -18,3 +18,43 @@ let a: FeatureOptions = {
   newUserProfile: true,
 };
 
+console.log(a);
+
+// mapping modifiers
+// +/- property properties to change the properies' properties
+
+type CreateMutable<Type> = {
+  -readonly [Property in keyof Type]: Type[Property];
+};
+
+type LockedAccount = {
+  readonly id: string;
+  readonly name: string;
+};
+
+type UnlockedAccount = CreateMutable<LockedAccount>;
+
+/*&type UnlockedAccount = {
+    id: string;
+    name: string;
+}*/
+
+type Concrete<Type> = {
+  [Property in keyof Type]-?: Type[Property];
+};
+
+type MaybeUser = {
+  id: string;
+  name?: string;
+  age?: number;
+};
+
+type User = Concrete<MaybeUser>;
+/*
+ * type User = {
+    id: string;
+    name: string;
+    age: number;
+    }
+ */
+
