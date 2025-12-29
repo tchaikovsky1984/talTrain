5862e1956166df8fa9ad044d4ad6d5adf1b3c863
Anshul Kumar <anshulkumargore@gmail.com>
2025-12-29 17:04:44 +0530

    redux/saga

--- temporal/prac_1/working.py
+++ temporal/prac_1/working.py
@@ -9,7 +9,14 @@ from act import failure_prone_function
 
 async def main():
     client = await Client.connect("localhost:7233", namespace="default")
-    worker = Worker(client, task_queue="greeting-tasks", workflows=[GreetSomeone], activities=[failure_prone_function])
+
+    worker = Worker(
+            client, 
+            task_queue="greeting-tasks", 
+            workflows=[GreetSomeone], 
+            activities=[failure_prone_function]
+            )
+
     await worker.run()
 
 if __name__ == "__main__":
